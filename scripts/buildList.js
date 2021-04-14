9358f28ec1552aaec3040608fad8e80132a5ebb4
Ian Macalinao <github@igm.pub>
2021-04-14 00:15:20 -0500

    Properly sort token list

--- scripts/buildList.js
+++ scripts/buildList.js
@@ -1,40 +1,72 @@
 const alfajores = require("../src/alfajores.tokens.json");
 const mainnet = require("../src/mainnet.tokens.json");
 const baklava = require("../src/baklava.tokens.json");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const package = require("../package.json");
 const version = package.version.split(".");
+const process = require("process");
 
-const result = {
-  name: "Ubeswap",
-  logoURI:
-    "https://raw.githubusercontent.com/Ubeswap/default-token-list/master/logo.svg",
-  keywords: ["ubeswap", "defi"],
-  timestamp: new Date().toISOString(),
-  tokens: [
-    ...alfajores.map((el) => ({ ...el, chainId: 44787 })),
-    ...baklava.map((el) => ({ ...el, chainId: 62320 })),
-    ...mainnet.map((el) => ({ ...el, chainId: 42220 })),
-  ]
-    .filter((el) => !el.isHidden)
-    .map((el) => {
-      return {
-        ...el,
-        decimals: el.decimals || 18,
-        logoURI:
-          el.logoURI ||
-          `https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_${el.symbol}.png`,
-      };
-    }),
-  version: {
-    major: parseInt(version[0]),
-    minor: parseInt(version[1]),
-    patch: parseInt(version[2]),
-  },
+const LOGO_URI_BASE =
+  "https://raw.githubusercontent.com/ubeswap/default-token-list/master";
+
+const main = async () => {
+  const result = {
+    name: "Ubeswap",
+    logoURI:
+      "https://raw.githubusercontent.com/Ubeswap/default-token-list/master/logo.svg",
+    keywords: ["ubeswap", "defi"],
+    timestamp: new Date().toISOString(),
+    tokens: await Promise.all(
+      [
+        ...alfajores.map((el) => ({ ...el, chainId: 44787 })),
+        ...baklava.map((el) => ({ ...el, chainId: 62320 })),
+        ...mainnet.map((el) => ({ ...el, chainId: 42220 })),
+      ]
+        .filter((el) => !el.isHidden)
+        .map(async (el) => {
+          const logoURI =
+            el.logoURI || `${LOGO_URI_BASE}/assets/asset_${el.symbol}.png`;
+
+          // Validate
+          if (logoURI.startsWith(LOGO_URI_BASE)) {
+            if (logoURI !== `${LOGO_URI_BASE}/assets/asset_${el.symbol}.png`) {
+              throw new Error(
+                `unexpected logo URI: ${el.symbol} but got ${logoURI.slice(
+                  logoURI.lastIndexOf("/")
+                )}`
+              );
+            }
+            const stat = await fs.stat(
+              `${__dirname}/..${logoURI.slice(LOGO_URI_BASE.length)}`
+            );
+            if (!stat.isFile()) {
+              throw new Error(
+                `logo for ${el.address} on ${el.chainId} does not exist`
+              );
+            }
+          }
+          return {
+            ...el,
+            decimals: el.decimals || 18,
+            logoURI: el.logoURI,
+          };
+        })
+    ),
+    version: {
+      major: parseInt(version[0]),
+      minor: parseInt(version[1]),
+      patch: parseInt(version[2]),
+    },
+  };
+
+  await fs.writeFile(
+    __dirname + "/../ubeswap.token-list.json",
+    JSON.stringify(result, null, 2)
+  );
 };
 
-fs.writeFileSync(
-  __dirname + "/ubeswap.token-list.json",
-  JSON.stringify(result, null, 2)
-);
+main().catch((err) => {
+  console.error("Error", err);
+  process.exit(1);
+});
--- scripts/cleanupTokens.js
+++ scripts/cleanupTokens.js
@@ -1,11 +1,13 @@
 const { getAddress } = require("@ethersproject/address");
 const fs = require("fs/promises");
+const process = require("process");
 
 const cleanupTokens = async (network) => {
   const file = `${__dirname}/../src/${network}.tokens.json`;
   const tokens = require(file).sort((a, b) =>
     a.address.toLowerCase().localeCompare(b.address.toLowerCase())
   );
+
   await fs.writeFile(
     file,
     JSON.stringify(
@@ -14,8 +16,13 @@ const cleanupTokens = async (network) => {
   );
 };
 
-(async () => {
+const main = async () => {
   console.log("Fixing addresses...");
   await Promise.all(["alfajores", "mainnet", "baklava"].map(cleanupTokens));
   console.log("Fixed and files overwritten");
-})();
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
